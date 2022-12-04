3e8ed47fb76f25c27bbb4658c2c0a817e67c5bba
IKITsila <vklr4id@yandex.ru>
2022-12-04 23:17:08 +0700

    fast pow

---
+++ fast_pow.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double fast_pow(double num, unsigned int power) {
+	if (power == 0) {
+		return 1;
+	}
+
+	if (power % 2 == 0){
+		return fast_pow(num * num, power / 2);
+	}
+
+	return num * fast_pow(num, power - 1);
+}
+
+int main()
+{
+	double num;
+	unsigned int power;
+
+	cin >> num >> power;
+	double res = fast_pow(num, power);
+	cout << res;
+	
+
+	return 0;
+}
