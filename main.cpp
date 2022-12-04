dce17459f6e333f2c8f957bf37b6e9a77e402c0c
IKITsila <vklr4id@yandex.ru>
2022-12-04 23:08:47 +0700

    my pow

---
+++ main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double my_pow(double num, int degree)
+{
+    if (degree == 0) {
+        return 1;
+    }
+
+    double res = 1;
+   
+    for (int i = 0; i < degree; i++) {
+        res *= num;
+    }
+    
+    return res;
+}
+
+int main()
+{
+    int degree;
+    double indicator;
+
+    cin >> indicator >> degree;
+    double number = my_pow(indicator, degree);
+
+    cout << number;
+
+    return 0;
+}
